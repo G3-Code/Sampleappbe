aa1dd01f438e461d133dccad85a37ccb6549518f
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-01-28 06:36:32 -0600

    Updates for PORT numbers

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const port = 3333;
+const port = process.env.PORT || 3333;
 
 const server = express();
 server.use(express.json());
