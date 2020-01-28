b00b5117cef99d07ae608bb0bb449c6400ed925d
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-01-28 07:23:56 -0600

    Added test data

--- server.js
+++ server.js
@@ -19,83 +19,139 @@ server.get("/", (req, res) => {
 let forecast = [
   {
     city: "Chennai",
-    day: "Mon",
-    high: "65",
-    low: "45",
-    message: "Cloudy",
+    day: "Tue",
+    high: "91",
+    low: "71",
+    message: "Sunny",
     id: 0
   },
   {
     city: "Chennai",
-    day: "Tue",
-    high: "63",
-    low: "45",
-    message: "Sunny",
+    day: "Wed",
+    high: "93",
+    low: "68",
+    message: "Cloudy",
     id: 1
   },
   {
     city: "Chennai",
-    day: "Wed",
-    high: "23",
-    low: "45",
-    message: "Cloudy",
+    day: "Thu",
+    high: "95",
+    low: "70",
+    message: "Sunny",
     id: 2
   },
   {
     city: "Chennai",
-    day: "Thu",
-    high: "45",
-    low: "45",
+    day: "Fri",
+    high: "94",
+    low: "71",
     message: "Sunny",
     id: 3
   },
   {
-    city: "Dallas",
-    day: "Tue",
-    high: "63",
-    low: "45",
+    city: "Chennai",
+    day: "Sat",
+    high: "92",
+    low: "71",
     message: "partlysunny",
     id: 4
   },
   {
     city: "Dallas",
-    day: "Wed",
-    high: "23",
-    low: "45",
-    message: "Sunny",
+    day: "Tue",
+    high: "51",
+    low: "43",
+    message: "Rainny",
     id: 5
   },
   {
-    city: "Bombay",
-    day: "Thu",
-    high: "45",
-    low: "45",
-    message: "Rainny",
+    city: "Dallas",
+    day: "Wed",
+    high: "47",
+    low: "39",
+    message: "Cloudy",
     id: 6
   },
   {
-    city: "Chennai",
-    day: "Tue",
-    high: "63",
-    low: "45",
+    city: "Dallas",
+    day: "Thu",
+    high: "48",
+    low: "40",
     message: "Cloudy",
     id: 7
   },
   {
-    city: "Chennai",
-    day: "Wed",
-    high: "23",
-    low: "45",
+    city: "Dallas",
+    day: "Fri",
+    high: "56",
+    low: "36",
     message: "Sunny",
     id: 8
   },
   {
-    city: "Austin",
-    day: "Thu",
-    high: "45",
-    low: "45",
+    city: "Dallas",
+    day: "Sat",
+    high: "64",
+    low: "42",
     message: "Sunny",
     id: 9
+  },
+  {
+    city: "Austin",
+    day: "Tue",
+    high: "71",
+    low: "44",
+    message: "Rainny",
+    id: 10
+  },
+  {
+    city: "Austin",
+    day: "Wed",
+    high: "62",
+    low: "43",
+    message: "Sunny",
+    id: 11
+  },
+  {
+    city: "Austin",
+    day: "Thu",
+    high: "54",
+    low: "44",
+    message: "Cloudy",
+    id: 12
+  },
+  {
+    city: "Austin",
+    day: "Fri",
+    high: "61",
+    low: "41",
+    message: "Sunny",
+    id: 13
+  },
+  {
+    city: "Austin",
+    day: "Sat",
+    high: "70",
+    low: "46",
+    message: "Sunny",
+    id: 14
+  },
+  {
+    city: "London",
+    day: "Tue",
+    high: "45",
+    low: "39",
+    message: "Sunny",
+    id: 15
+  },
+  {
+    city: "London",
+    day: "Wed",
+    high: "50",
+    low: "45",
+    message: "partlysunny",
+    id: 16
   }
 ];
 
