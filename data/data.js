d798db176358de773238bd7546bfd9b6e670787a
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-01-28 11:50:32 -0600

    Moved data to another file

---
+++ data/data.js
@@ -0,0 +1,140 @@
+let forecast = [
+  {
+    city: "Chennai",
+    day: "Tue",
+    high: "91",
+    low: "71",
+    message: "Sunny",
+    id: 0
+  },
+  {
+    city: "Chennai",
+    day: "Wed",
+    high: "93",
+    low: "68",
+    message: "Cloudy",
+    id: 1
+  },
+  {
+    city: "Chennai",
+    day: "Thu",
+    high: "95",
+    low: "70",
+    message: "Sunny",
+    id: 2
+  },
+  {
+    city: "Chennai",
+    day: "Fri",
+    high: "94",
+    low: "71",
+    message: "Sunny",
+    id: 3
+  },
+  {
+    city: "Chennai",
+    day: "Sat",
+    high: "92",
+    low: "71",
+    message: "partlysunny",
+    id: 4
+  },
+  {
+    city: "Dallas",
+    day: "Tue",
+    high: "51",
+    low: "43",
+    message: "Rainy",
+    id: 5
+  },
+  {
+    city: "Dallas",
+    day: "Wed",
+    high: "47",
+    low: "39",
+    message: "Cloudy",
+    id: 6
+  },
+  {
+    city: "Dallas",
+    day: "Thu",
+    high: "48",
+    low: "40",
+    message: "Cloudy",
+    id: 7
+  },
+  {
+    city: "Dallas",
+    day: "Fri",
+    high: "56",
+    low: "36",
+    message: "Sunny",
+    id: 8
+  },
+  {
+    city: "Dallas",
+    day: "Sat",
+    high: "64",
+    low: "42",
+    message: "Sunny",
+    id: 9
+  },
+  {
+    city: "Austin",
+    day: "Tue",
+    high: "71",
+    low: "44",
+    message: "Rainy",
+    id: 10
+  },
+  {
+    city: "Austin",
+    day: "Wed",
+    high: "62",
+    low: "43",
+    message: "Sunny",
+    id: 11
+  },
+  {
+    city: "Austin",
+    day: "Thu",
+    high: "54",
+    low: "44",
+    message: "Cloudy",
+    id: 12
+  },
+  {
+    city: "Austin",
+    day: "Fri",
+    high: "61",
+    low: "41",
+    message: "Sunny",
+    id: 13
+  },
+  {
+    city: "Austin",
+    day: "Sat",
+    high: "70",
+    low: "46",
+    message: "Sunny",
+    id: 14
+  },
+  {
+    city: "London",
+    day: "Tue",
+    high: "45",
+    low: "39",
+    message: "Sunny",
+    id: 15
+  },
+  {
+    city: "London",
+    day: "Wed",
+    high: "50",
+    low: "45",
+    message: "partlysunny",
+    id: 16
+  }
+];
+
+module.exports = forecast;
