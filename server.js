dd332711020c01d11773f30a11b698f13cfa4d58
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-01-28 11:53:32 -0600

    Added developer comments

--- server.js
+++ server.js
@@ -22,10 +22,17 @@ server.get("/", (req, res) => {
   res.status(200).json({ message: "The Weather server is up and running!" });
 });
 
+/**
+ * Gets the forecast for a particular place
+ */
 server.get("/forecast/:place", (req, res) => {
+  //Gets the name of the place
   let { place } = req.params;
 
+  //Filters the forecast for that place
   let myForecasts = forecast.filter(forecast => forecast.city == place);
+
+  //If forecast is available sends the forecast. Otherwise sends the message.
   if (myForecasts.length > 0) {
     myForecasts.length = myForecasts.length > 5 ? 5 : myForecasts.length;
     res.status(200).json({
@@ -40,7 +47,11 @@ server.get("/forecast/:place", (req, res) => {
   }
 });
 
+/**
+ * Gets the distinct list of cities for the drop down.
+ */
 server.get("/cities", (req, res) => {
+  // Gets the unique list of cities
   let myCities = [...new Set(forecast.map(item => item.city))];
   res.status(200).json(myCities);
 });
